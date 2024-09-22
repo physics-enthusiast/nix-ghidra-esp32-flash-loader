95213687a418c89ad0fd86426ee0cd17b3cc29f5
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-09-22 17:51:11 +0800

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,3 +1,3 @@
 { pkgs ? import <nixpkgs> {} }: rec {
-  ghidra-esp32-flash-loader = pkgs.callPackage ./ghidra-esp32-flash-loader.nix { inherit (pkgs.ghidra) buildGhidraExtension };
+  ghidra-esp32-flash-loader = pkgs.callPackage ./ghidra-esp32-flash-loader.nix { inherit (pkgs.ghidra) buildGhidraExtension; };
 }
