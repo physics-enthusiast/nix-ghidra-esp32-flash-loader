49ab02f009490d195a0138dcb8c51173a039f333
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-09-29 20:40:07 +0800

    Update ghidra-esp32-flash-loader.nix

--- ghidra-esp32-flash-loader.nix
+++ ghidra-esp32-flash-loader.nix
@@ -7,8 +7,8 @@ let
     owner = "physics-enthusiast";
     repo = "ghidra-esp32-flash-loader";
     #rev = "v${version}";
-    rev = "072017eaf9c1c46d47d7b1ffeb33dfd1851ca58e";
-    hash = "sha256-BLE7Bdfd5KcQcFYI7Z7KbMMLFXLYUQzQ9YUyQ6Xtf98=";
+    rev = "1c5767f2f34653b0b563e783bd906caba097dab8";
+    hash = "sha256-BLE7Bdfd5KcQcFYI7Z7KbaMLFXLYUQzQ9YUyQ6Xtf98=";
     fetchSubmodules = true;
   };
 in
